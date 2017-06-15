689dc121e6919b449d7542cee635e8110ebdaa59
Rodrigo Gonçalves <rodrigosg2000@gmail.com>
2017-06-15 17:24:44 -0300

    4.1

--- client/js/main.js
+++ client/js/main.js
@@ -1,27 +1,54 @@
-var frase = $('.frase').text();
-var numPalavras = frase.split(" ").length;
-var tamanhoFrase = $("#tamanho-frase");
-tamanhoFrase.text(numPalavras);
-
+var tempoInicial = 3;
 var campo = $(".campo-digitacao");
-campo.on("input", function() {
-	var conteudo = campo.val();
 
-	var qtdPalavras = conteudo.split(/\S+/).length - 1;
-	$("#contador-palavras").text(qtdPalavras);
-
-	var qtdCaracteres = conteudo.length;
-	$("#contador-caracteres").text(qtdCaracteres);
+// $(document).ready(function() {
+$(function() {
+	atualizaTamanhoFrase();
+	inicializaContadores();
+	inicializaCronometro();
+	$("#botao-reiniciar").click(reiniciarJogo);
 });
 
-var tempoRestante = $("#tempo-digitacao").text();
-campo.one("focus", function() {
-	var cronometroID = setInterval(function() {
-		if (tempoRestante <= 0) {
-			campo.attr("disabled", true);
-			clearInterval(cronometroID);
-		}
-		$("#tempo-digitacao").text(tempoRestante);
-		tempoRestante--;
-	}, 1000);
-})
+function atualizaTamanhoFrase() {
+	var frase = $(".frase").text();
+	var numPalavras = frase.split(" ").length;
+	var tamanhoFrase = $("#tamanho-frase");
+	tamanhoFrase.text(numPalavras);
+}
+
+function inicializaContadores() {
+
+	campo.on("input", function() {
+		var conteudo = campo.val();
+
+		var qtdPalavras = conteudo.split(/\S+/).length - 1;
+		$("#contador-palavras").text(qtdPalavras);
+
+		var qtdCaracteres = conteudo.length;
+		$("#contador-caracteres").text(qtdCaracteres);
+	});
+}
+
+function inicializaCronometro() {
+	var tempoRestante = $("#tempo-digitacao").text();
+	campo.one("focus", function() {
+		var cronometroID = setInterval(function() {
+			if (tempoRestante <= 0) {
+				campo.attr("disabled", true);
+				clearInterval(cronometroID);
+			}
+			$("#tempo-digitacao").text(tempoRestante);
+			tempoRestante--;
+		}, 1000);
+	});
+}
+
+function reiniciarJogo() {
+	campo.attr("disabled", false);
+	campo.val("");
+	$("#tempo-digitacao").text(tempoInicial);
+	$("#contador-caracteres").text("0");
+	$("#contador-palavras").text("0");
+
+	inicializaCronometro();
+}
