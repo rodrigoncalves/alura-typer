c8329c1d4b5fc559cff6d3996da3ceb448a85a63
Rodrigo Gonçalves <rodrigosg2000@gmail.com>
2017-06-20 22:19:31 -0300

    6.3

--- client/js/main.js
+++ client/js/main.js
@@ -1,4 +1,4 @@
-var tempoInicial = 5;
+var tempoInicial = 3;
 var campo = $(".campo-digitacao");
 
 // $(document).ready(function() {
@@ -83,14 +83,36 @@ function inicializaMarcadores() {
 
 
 function inserePlacar() {
+	var tbody = $(".placar").find("tbody");
 	var usuario = "Rodrigo";
 	var numPalavras = $("#contador-palavras").text();
 
-	var tbody = $(".placar").find("tbody");
-	var htmlData =
-		"<tr>"+
-			"<td>"+usuario+"</td>"+
-			"<td>"+numPalavras+"</td>"+
-		"</tr>";
-	tbody.prepend(htmlData);
+	var linha = novaLinha(usuario, numPalavras);
+	linha.find(".botao-remover").click(removeLinha);
+
+	tbody.prepend(linha);
+}
+
+function novaLinha(usuario, numPalavras) {
+	var linha = $("<tr>");
+	var colunaUsuario = $("<td>").text(usuario);
+	var colunaPalavras = $("<td>").text(numPalavras);
+
+	var colunaRemover = $("<td>");
+	var link = $("<a>").addClass("botao-remover").attr("href", "#");
+	var icon = $("<i>").addClass('small material-icons').text("delete");
+
+	link.append(icon);
+	colunaRemover.append(link);
+
+	linha.append(colunaUsuario);
+	linha.append(colunaPalavras);
+	linha.append(colunaRemover);
+
+	return linha;
+}
+
+function removeLinha() {
+	event.preventDefault();
+	$(this).parent().parent().remove();
 }
